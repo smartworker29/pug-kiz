7139bb35148cc3ae656933dc2c137503207e6996
Denys Vladymyrov <link_amd@yahoo.com>
2020-06-29 12:10:24 +0300

    fixed "license-type"

--- src/widgets/license-type/license-type.js
+++ src/widgets/license-type/license-type.js
@@ -4,35 +4,49 @@ export default function licenseSlider() {
 
   return {
     init: () => {
-      console.log('init')
-      var license = new Swiper('.license-swiper', {
-        spaceBetween: 20,
-        loop: true,
-        speed: 1500,
-        centeredSlides: true,
-        navigation: {
-          nextEl: '.license-swiper .swiper-button-next',
-          prevEl: '.license-swiper .swiper-button-prev',
-        },
-        pagination: {
-          el: '.swiper-pagination',
-        },
-        breakpoints: {
-          320:{
-            slidesPerView: 1,
-          },
-          991: {
-            slidesPerView: 2.3,
+      var mySwiper = undefined;
+      function initSwiper() {
+        var screenWidth = $(window).width();
+        if(screenWidth < 992 && mySwiper == undefined) {
+          mySwiper = new Swiper('.swiper-container.license-swiper', {
+            loop: true,
+            speed: 1500,
+            centeredSlides: true,
             navigation: {
               nextEl: '.license-swiper .swiper-button-next',
               prevEl: '.license-swiper .swiper-button-prev',
             },
-            // loopedSlides: 1,
-          },
+            pagination: {
+              el: '.swiper-pagination',
+              type: 'bullets',
+              clickable: true,
+            },
+            breakpoints: {
+              320:{
+                slidesPerView: 1,
+              },
+              576: {
+                slidesPerView: 1.5,
+              },
+              768: {
+                slidesPerView: 1.7,
+              },
 
+            }
+          });
+        } else if (screenWidth > 991 && mySwiper != undefined) {
+          mySwiper.destroy();
+          mySwiper = undefined;
+          jQuery('.license-swiper .swiper-wrapper').removeAttr('style');
+          jQuery('.license-swiper .swiper-slide').removeAttr('style');
         }
-      }, 500);
-    },
+      }
 
+      initSwiper();
+
+      $(window).on('resize', function(){
+        initSwiper();
+      });
+    }
   }
 }
