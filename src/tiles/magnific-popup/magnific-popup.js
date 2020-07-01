f6b00f0d71af3afcd10f2ae0489f1596b4cb012a
solidperez <hitgoal312@gmail.com>
2020-07-01 08:54:06 +0400

    Magnific popup length

--- src/tiles/magnific-popup/magnific-popup.js
+++ src/tiles/magnific-popup/magnific-popup.js
@@ -1,6 +1,7 @@
 import "magnific-popup/dist/jquery.magnific-popup"
 
 $(document).ready(function() {
+  if($('.image-popup').length > 0){
     $('.image-popup').magnificPopup({ 
         type: 'image',
         zoom: {
@@ -11,5 +12,5 @@ $(document).ready(function() {
             }
           }
     });
-
+  }
 });
