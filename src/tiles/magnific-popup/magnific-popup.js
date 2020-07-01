02aea15406ac5c878c26db5ecfe4fd2ef8e97f5c
solidperez <hitgoal312@gmail.com>
2020-07-01 13:04:01 +0400

    Full width Carousel

--- src/tiles/magnific-popup/magnific-popup.js
+++ src/tiles/magnific-popup/magnific-popup.js
@@ -1,14 +1,14 @@
 import "magnific-popup/dist/jquery.magnific-popup"
 
 $(document).ready(function() {
-  if($('.image-popup').length > 0){
-    $('.image-popup').magnificPopup({ 
+  if($('.zoom-icon').length > 0){
+    $('.zoom-icon').magnificPopup({ 
         type: 'image',
         zoom: {
             enabled: true,
             duration: 300, // don't foget to change the duration also in CSS
             opener: function(element) {
-              return element.find('img');
+              return element;
             }
           }
     });
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackMd5Hash = require("webpack-md5-hash");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-// const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
 const fs = require("fs");
 const webpack = require('webpack');
 
@@ -66,15 +65,6 @@ module.exports = {
         test: /\.pug$/,
         use: ['html-loader?interpolate', 'pug-html-loader?pretty=true']
       }
-      // {
-      //   test: /\.(jpeg|jpg|png|gif|svg)$/i,
-      //   loader: 'file-loader',
-
-      //   options: {
-      //       name: '[name].[ext]?[hash]',
-      //       outputPath: './assets/images/webp'
-      //   }
-      // }
     ]
   },
   plugins: [
@@ -82,11 +72,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "style.[contenthash].css"
     }),
-    // new ImageminWebpWebpackPlugin({
-    //     detailedLogs: true,
-    //     overrideExtension: true
-    // }),
-
     ...pages.map(
       page =>
         new HtmlWebpackPlugin({
