51adee662d982a43cf0081280881c35601faa7b7
solidperez <hitgoal312@gmail.com>
2020-07-07 17:17:13 +0400

    Form

--- src/widgets/support/support.js
+++ src/widgets/support/support.js
@@ -1,46 +1,75 @@
-// let hasVal = document.querySelectorAll('form .support-input');
+let hasVal = document.querySelectorAll('form .form__item input:not([type="hidden"]), form .form__item textarea');
 
-// hasVal.forEach(el => {
-//   el.addEventListener('blur', function () {
-//         let val = this.value.trim();
+hasVal.forEach(el => {
+  el.addEventListener('blur', function () {
+        let val = this.value.trim();
+        if (val.length !== 0) {
+          this.classList.add('has-value');
+        } else {
+          this.classList.remove('has-value');
+        }
+      }
+  );
+});
 
-//         if (val.length !== 0) {
-//           this.classList.add('has-value');
-//         } else {
-//           this.classList.remove('has-value');
-//         }
-//       }
-//   );
+$('.text-texarea').on('blur', function(e) {
+  $('form button[type=submit]').addClass('btn-secondary');
+  if(e.target.value.trim() === ''){
+    $(this).removeClass('valid');
+    $(this).addClass('error');
+  }
+  else{
+    $(this).addClass('valid');
+    $(this).removeClass('error');
+  }
+});
 
-// });
+$('.text-input').on('blur', function(e) {
+    $('form button[type=submit]').addClass('btn-secondary');
+    if(e.target.value.trim() === ''){
+        $(this).removeClass('valid');
+        $(this).addClass('error');
+    }
+    else{
+        $(this).addClass('valid');
+        $(this).removeClass('error');
+    }
+});
 
-// $('.text-input').on('blur', function(e) {
-//   $('form button[type=submit]').addClass('btn-secondary');
-//   if(e.target.value.trim() === ''){
-//     $(this).removeClass('valid');
-//     $(this).addClass('error');
-//   }
-//   else{
-//     $(this).addClass('valid');
-//     $(this).removeClass('error');
-//   }
-// });
+$('.email-input').on('blur', function(e) {
+  const regExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  let result = regExp.test(e.target.value);
+  if(!result){
+    $(this).removeClass('valid');
+    $(this).addClass('error');
+  }
+  else{
+    $(this).addClass('valid');
+    $(this).removeClass('error');
+  }
+});
 
-// $('.email-input').on('blur', function(e) {
-//   const regExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+$('.custom-form .submit-button').on('click', function(e){
+    e.preventDefault();
+    var condition = true;
+    var inputele = $(this).parents('.custom-form').find('.form__item input:not([type="hidden"])');
+    var textareaele = $(this).parents('.custom-form').find('.form__item textarea');
+    inputele.each(function(e,ele){
+        if($(ele).hasClass('error') && $(ele).val()){
+            condition = false
+        }
+    })
+    textareaele.each(function(e,ele){
+        if($(ele).hasClass('error') && $(ele).val()){
+            condition = false
+        }
+    })
 
-//   let result = regExp.test(e.target.value);
-
-//   if(!result){
-//     $(this).removeClass('valid');
-//     $(this).addClass('error');
-//   }
-//   else{
-//     $(this).addClass('valid');
-//     $(this).removeClass('error');
-//   }
-
-// });
+    if(condition == true){
+        alert();
+        $(this).parents('.custom-form').submit();
+    }
+})
 
 
 // $('.modal').on('hidden.bs.modal', function (e) {
