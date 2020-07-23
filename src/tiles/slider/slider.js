603c1485b11f295e58afce7716178983966455c4
solidperez <hitgoal312@gmail.com>
2020-07-23 11:20:01 +0400

    [FIX] - Carousel Navigation and Pagination Fixes

--- src/tiles/slider/slider.js
+++ src/tiles/slider/slider.js
@@ -4,78 +4,45 @@ export default function swiperCustom() {
 
     return {
         init: () => {
-            var sliders = $('.swiper-custom .swiper-wrapper')
-        //     if(sliders){
-        //     $('.swiper-custom .swiper-wrapper').each(function(i,items){
-        //         var items = $(items)
-        //         var slideitems = items.data('slide');
-        //         var itemSpacing = items.data('space');
-        //             var swiperCustom = new Swiper(items.parent(), {
-        //                 spaceBetween:itemSpacing,
-        //                 loop: false,
-        //                 speed: 1500,
-        //                 centeredSlides: false,
-        //                 slidesPerView: slideitems,
-        //                 pagination: '.swiper-pagination',
-        //                 paginationClickable: true,
-        //                 nextButton: '.swiper-button-next',
-        //                 prevButton: '.swiper-button-prev',
-        //                 // pagination: {
-        //                 //     el: '.swiper-pagination',
-        //                 // },
-        //                 // navigation: {
-        //                 //     nextEl: '.swiper-custom .swiper-button-next',
-        //                 //     prevEl: '.swiper-custom .swiper-button-prev',
-        //                 // },
-        //                 breakpoints: {
-        //                     320: {
-        //                         slidesPerView: 1,
-        //                     },
-        //                     991: {
-        //                         slidesPerView: slideitems,
-        //                         // loopedSlides: 1,
-        //                     },
-        //                 },
+            var sliders = $('.swiper-custom')
+            if (sliders) {
+                [].map.call(sliders, function (items, index) {
+                    debugger
+                    var classlist = items.classList[0]
+                    var item = $(items).find('.swiper-pagination').addClass(classlist + index)
+                    var nextitem = $(items).find('.swiper-button-next').addClass(classlist + index)
+                    var previtem = $(items).find('.swiper-button-prev').addClass(classlist + index)
+                    var slideitems = $(items).find('.swiper-wrapper').data('slide');
+                    var itemSpacing = $(items).find('.swiper-wrapper').data('space');
+                    var swiperCustom = new Swiper(items, {
+                        spaceBetween: itemSpacing,
+                        loop: false,
+                        speed: 1500,
+                        centeredSlides: false,
+                        slidesPerView: slideitems,
+                        pagination: {
+                            el: item,
+                          },
+                        navigation: {
+                            nextEl: nextitem,
+                            prevEl: previtem,
+                        },
 
-        //             });
-        //             if(items.find('.swiper-slide').length < 2){
-        //                 items.parent().addClass('disable-carousel');
-        //             }
-                
-        //     })
-        // }
-            if(sliders){
-                [].map.call(sliders, function (items) {
-                    var slideitems = $(items).data('slide');
-                    var itemSpacing = $(items).data('space');
-                    setTimeout(function(){
-                        var swiperCustom = new Swiper($(items).parent(), {
-                            spaceBetween:itemSpacing,
-                            loop: false,
-                            speed: 1500,
-                            centeredSlides: false,
-                            slidesPerView: slideitems,
-                            pagination: '.swiper-pagination',
-                            paginationClickable: true,
-                            nextButton: '.swiper-button-next',
-                            prevButton: '.swiper-button-prev',
-                           
-                            breakpoints: {
-                                320: {
-                                    slidesPerView: 1,
-                                },
-                                991: {
-                                    slidesPerView: slideitems,
-                                    // loopedSlides: 1,
-                                },
+                        breakpoints: {
+                            320: {
+                                slidesPerView: 1,
                             },
-    
-                        });
-                        if($(items).find('.swiper-slide').length < 2){
-                            $(items).parent().addClass('disable-carousel');
-                        }
-                    },500)
-                    
+                            991: {
+                                slidesPerView: slideitems,
+                            },
+                        },
+
+                    });
+                    if ($(items).find('.swiper-slide').length < 2) {
+                        $(items).parent().addClass('disable-carousel');
+                    }
+
+
                 });
             }
         },
